cab039c70bf68d4d844f115d64939dd7b267f4da
Timothy Woodby <timwoodby@gmail.com>
2023-03-11 19:14:19 -0500

    added sliced functions

--- slice.go
+++ slice.go
@@ -8,3 +8,16 @@ func SliceContains[T comparable](needle T, haystack []T) bool {
 	}
 	return false
 }
+
+func SliceRemoveValue[T comparable](list []T, value T) []T {
+	for idx, v := range list {
+		if v == value {
+			return SliceRemoveIndex(list, idx)
+		}
+	}
+	return list
+}
+
+func SliceRemoveIndex[T comparable](list []T, idx int) []T {
+	return append(list[:idx], list[idx+1:]...)
+}
--- slice_test.go
+++ slice_test.go
@@ -34,3 +34,79 @@ func TestSliceContains(t *testing.T) {
 	)
 
 }
+
+func TestSliceRemoveIndex(t *testing.T) {
+	type args[T comparable] struct {
+		list []T
+		idx  int
+	}
+	type testCase[T comparable] struct {
+		name string
+		args args[T]
+		want []T
+	}
+	tests := []testCase[string]{
+		{name: "level0", args: args[string]{list: []string{"aaa", "abc", "abb"}, idx: 0}, want: []string{"abc", "abb"}},
+		{name: "level1", args: args[string]{list: []string{"aaa", "abc", "abb"}, idx: 1}, want: []string{"aaa", "abb"}},
+		{name: "level2", args: args[string]{list: []string{"aaa", "abc", "abb"}, idx: 2}, want: []string{"aaa", "abc"}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equalf(t, tt.want, SliceRemoveIndex(tt.args.list, tt.args.idx), "SliceRemoveIndex(%v, %v)", tt.args.list, tt.args.idx)
+		})
+	}
+}
+
+func TestSliceRemoveValue(t *testing.T) {
+	type args[T comparable] struct {
+		list  []T
+		value T
+	}
+	type testCase[T comparable] struct {
+		name string
+		args args[T]
+		want []T
+	}
+	tests := []testCase[int]{
+		{
+			name: "test 1",
+			args: args[int]{list: []int{1, 2, 3, 4, 5, 6}, value: 1},
+			want: []int{2, 3, 4, 5, 6},
+		},
+		{
+			name: "test 2",
+			args: args[int]{list: []int{1, 2, 3, 4, 5, 6}, value: 2},
+			want: []int{1, 3, 4, 5, 6},
+		},
+		{
+			name: "test 3",
+			args: args[int]{list: []int{1, 2, 3, 4, 5, 6}, value: 3},
+			want: []int{1, 2, 4, 5, 6},
+		},
+		{
+			name: "test 4",
+			args: args[int]{list: []int{1, 2, 3, 4, 5, 6}, value: 4},
+			want: []int{1, 2, 3, 5, 6},
+		},
+		{
+			name: "test 5",
+			args: args[int]{list: []int{1, 2, 3, 4, 5, 6}, value: 5},
+			want: []int{1, 2, 3, 4, 6},
+		},
+		{
+			name: "test 6",
+			args: args[int]{list: []int{1, 2, 3, 4, 5, 6}, value: 6},
+			want: []int{1, 2, 3, 4, 5},
+		},
+		{
+			name: "test 77",
+			args: args[int]{list: []int{1, 2, 3, 4, 5, 6}, value: 77},
+			want: []int{1, 2, 3, 4, 5, 6},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equalf(t, tt.want, SliceRemoveValue(tt.args.list, tt.args.value), "SliceRemoveValue(%v, %v)", tt.args.list, tt.args.value)
+		})
+	}
+}
